1a9608cd56597551a88510e3be2077955b7a2303
dvinhliu <luuducvinh1392@gmail.com>
2024-09-13 00:46:40 +0700

    fix conflict

--- user/home.php
+++ user/home.php
@@ -4,11 +4,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <script src="assets/js/jquery-3.2.1.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="assets/css/slick.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/slick-theme.css" />
+
+    <!--  -->
+    <link rel="stylesheet" type="text/css" href="assets/css/Layout/style_header.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/Layout/style_footer.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/Home/style_home.css">
+    <title>Trang chủ</title>
 </head>
 
 <body>
     <?php
+    include("layout/header.php");
     include_once '../core/Connection.php';
     session_start();
     ?>
@@ -72,5 +84,10 @@
         </div>
     </div>
 
+    <?php
+    include("layout/footer.php");
+    ?>
+
 </body>
+
 </html>
